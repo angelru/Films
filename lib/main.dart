4a6b3e0b76062b998e1ed40104621fee62a38439
angelru <angel.valdeolmos@outlook.es>
2021-12-14 16:05:03 +0100

    providers and http

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
 import 'package:filmsapp/pages/pages.dart';
+import 'package:provider/provider.dart';
+import 'package:filmsapp/providers/movies_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false)
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  MoviesProvider() {
+    displayMovies();
+  }
+
+  displayMovies() async {}
+}
