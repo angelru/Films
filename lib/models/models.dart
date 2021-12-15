d8a62d10b92741e53e26b716d1a4c5a0cd1db61b
angelru <angel.valdeolmos@outlook.es>
2021-12-15 11:08:15 +0100

    Popular provider

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,2 +1,3 @@
 export 'package:filmsapp/models/movie.dart';
 export 'package:filmsapp/models/now_playing_response.dart';
+export 'package:filmsapp/models/popular_response.dart';
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -9,9 +9,11 @@ class MoviesProvider extends ChangeNotifier {
   final String _language = 'es-ES';
 
   List<Movie> movies = [];
+  List<Movie> popularMovies = [];
 
   MoviesProvider() {
     getMovies();
+    getPopularMovies();
   }
 
   getMovies() async {
@@ -21,6 +23,16 @@ class MoviesProvider extends ChangeNotifier {
     final response = await http.get(url);
     final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
     movies = nowPlayingResponse.results;
+    notifyListeners();
+  }
+
+  void getPopularMovies() async {
+    var url = Uri.https(_baseAddress, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(response.body);
+    popularMovies = [...popularResponse.results];
     notifyListeners();
   }
 }
